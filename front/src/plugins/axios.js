e98244a203deb533e26a76a8ed2ba23471d438e4
Charlot316 <2280583075@qq.com>
2021-12-04 17:42:03 +0800

    下单功能实现完毕 前端去掉了keep alive标签，因为会复用同一个组件，keep alive会导致看到的是以前的数据，但这导致了页面卡顿的问题，我到时候查查怎样在keep alive的情况下能清除缓存 新前端打包

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -1,6 +1,6 @@
-"use strict"
+'use strict'
 
-import axios from "axios"
+import axios from 'axios'
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || ''
@@ -8,40 +8,40 @@ import axios from "axios"
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 let config = {
-    baseURL: "http://localhost:8000/",
-  //baseURL: "http://42.192.60.125:80/",
+  //baseURL: "http://localhost:8000/",
+  baseURL: 'http://42.192.60.125:80/',
   timeout: 60 * 1000, // Timeout
-  withCredentials: true, // Check cross-site Access-Control
-};
+  withCredentials: true // Check cross-site Access-Control
+}
 
 // 创建Axios对象
 const Axios = axios.create(config)
 
 // 请求拦截器
 Axios.interceptors.request.use(
-    function (config) {
-        // Do something before request is sent
-        return config
-    },
-    function (error) {
-        // Do something with request error
-        return Promise.reject(error)
-    }
-);
+  function(config) {
+    // Do something before request is sent
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
 
 // 响应拦截器
 Axios.interceptors.response.use(
-    function (response) {
-        // Do something with response data
-        return response
-    },
-    function (error) {
-        // Do something with response error
-        return Promise.reject(error)
-    }
-);
+  function(response) {
+    // Do something with response data
+    return response
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
 
-export default (app) => {
-    // 挂载axios到Vue对象
-    app.config.globalProperties.$http = Axios
+export default app => {
+  // 挂载axios到Vue对象
+  app.config.globalProperties.$http = Axios
 }
